9258b25a52aa9f99479ab9db246def60ff9fddc3
Erkigmo <122825731+Erkigmo@users.noreply.github.com>
2023-03-17 18:00:46 -0400

    Add files via upload

---
+++ pywarfare/prototypes/fluids/nitrogen-oxide.lua
@@ -0,0 +1,15 @@
+FLUID {
+    type = "fluid",
+    name = "nitrogen-oxide",
+    icon = "__pywarfaregraphics__/graphics/icons/dynamite.png",
+	icon_size = 64,
+    default_temperature = 10,
+    base_color = {r = 0.340, g = 0.215, b = 0.172},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    subgroup = "py-warfare-liquids",
+    order = "a"
+}
